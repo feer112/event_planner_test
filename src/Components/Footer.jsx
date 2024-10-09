9ed4d4bbd3ca10855c7afd11cc4ff9c07d9e1ac2
Marc <marclawrence88@gmail.com>
2024-10-09 03:07:53 -0400

    Added details in EventPlanner.jsx and new Footer.jsx file

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+const Footer = () => {
+        return (
+    <>
+        <footer>
+            <p>&copy; asd123 LLC. All rights reserved</p>
+        </footer>
+    </>
+)}
+
+export default Footer
